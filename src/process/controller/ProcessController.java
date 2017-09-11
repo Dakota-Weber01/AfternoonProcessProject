68e180dedfc09d1971bdb6db7f627313b5695d34
Dakota Weber <dakota.weber01@gmail.com>
2017-09-11 12:21:14 -0600

    Added a new Play Doh Runner class

--- src/process/controller/ProcessController.java
+++ src/process/controller/ProcessController.java
@@ -1,12 +1,22 @@
 package	process.controller;
 
+import process.model.PlayDohCircle;
 public class ProcessController
 {
-	
+	public ProcessController()
+	{
+	}
 		public	void start()
 		{
 			System.out.println("This is a practice project.");
 			System.out.println("By practicing devolping muscle memory");
+			
+			PlayDohCircle firstCircle = new PlayDohCircle();
+			PlayDohCircle secondCircle;
+			secondCircle = new PlayDohCircle();
+			
+			System.out.println("Look I made a circle!");
+			System.out.println(firstCircle);
 		}
 }
 
---
+++ src/process/model/PlayDohCircle.java
@@ -0,0 +1,26 @@
+package process.model;
+
+public class PlayDohCircle
+{
+//Decleration Section
+	//Data Member Section
+	private int size; 
+{
+
+}
+public PlayDohCircle()
+{	
+	this.size = 5;
+	}
+	public PlayDohCircle(int size)
+	{
+			this.size = size;
+	
+	}
+public String toString()
+{
+	String description = "I am a circle, my size is " + size;
+	return description;
+}
+}
+
